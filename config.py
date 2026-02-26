f4045736d8fcb6b1ed62998017ddacb1bf958570
carzy-zala <mr.zala2003@gmail.com>
2026-02-26 09:55:55 +0000

    Number of parameters changes in the config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ DB_PATH = "data/hydrology.db"
 BASE_URL = "https://environment.data.gov.uk/hydrology"
 STATION_REF = "HIPPER_PARK ROAD BRIDGE_E_202312"
 LIMIT = 10
-PARAMETERS = ["dissolved oxygen", "conductivity","pH"]
+PARAMETERS = ["dissolved oxygen", "conductivity"]
 APPEND_MODE = False
